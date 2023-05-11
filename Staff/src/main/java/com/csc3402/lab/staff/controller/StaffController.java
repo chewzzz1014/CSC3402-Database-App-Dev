d091d81056ae666f20f6d869e7a184071eb68ebd
chewzzz <chewziqing@gmail.com>
2023-05-11 14:19:30 +0800

    Add controllers for update and delete

--- Staff/src/main/java/com/csc3402/lab/staff/controller/StaffController.java
+++ Staff/src/main/java/com/csc3402/lab/staff/controller/StaffController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/staffs")
@@ -38,5 +40,31 @@ public class StaffController {
         return "redirect:list";
     }
 
+    @GetMapping("update")
+    public String showUpdateMainForm(Model model) {
+        model.addAttribute("staffs", staffRepository.findAll());
+        return "choose-staff-to-update";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Staff staff = staffRepository.findById((int) id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid staff Id:" + id));
+        model.addAttribute("staff", staff);
+        model.addAttribute("departments", departmentRepository.findAll());
+        return "update-staff";
+    }
+
+    @PostMapping("update/{id}")
+    public String updateStaff(@PathVariable("id") long id, @Valid Staff staff, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            staff.setStaffId((int) id);
+            return "index";
+        }
+
+        model.addAttribute("staffs", staffRepository.findAll());
+        staffRepository.save(staff);
+        return "list-staff";
+    }
 
 }
