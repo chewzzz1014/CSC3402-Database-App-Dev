09591f65f64a07d089631600e6dd87cafafa6a0e
chewzzz <chewziqing@gmail.com>
2023-05-26 22:29:32 +0800

    Start manytomany project

---
+++ manytomany/src/main/java/com/csc3402/lab/manytomany/model/Course.java
@@ -0,0 +1,4 @@
+package com.csc3402.lab.manytomany.model;
+
+public class Course {
+}
---
+++ manytomany/src/main/java/com/csc3402/lab/manytomany/model/Student.java
@@ -0,0 +1,93 @@
+package com.csc3402.lab.manytomany.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+public class Student implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String grade;
+
+    @ManyToMany
+    @JoinTable(name = "students_courses",
+            joinColumns = {
+                    @JoinColumn(name = "student_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "course_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)})
+    private Set<Course> courses = new HashSet<>();
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    // setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                ", courses=" + courses +
+                '}';
+    }
+}
