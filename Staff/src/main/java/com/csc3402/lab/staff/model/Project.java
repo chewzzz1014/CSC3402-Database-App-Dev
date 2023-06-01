471a09f325c75c52ec60332fdadd4921321c9b6d
chewzzz <chewziqing@gmail.com>
2023-06-01 14:32:00 +0800

    add StaffProjectId and Project model

--- Staff/src/main/java/com/csc3402/lab/staff/model/Project.java
+++ Staff/src/main/java/com/csc3402/lab/staff/model/Project.java
@@ -1,4 +1,72 @@
 package com.csc3402.lab.staff.model;
+import java.util.*;
+import jakarta.persistence.*;
 
 public class Project {
+
+    public Project() {
+    }
+
+    public Project(Integer projectId, String projName, Integer cost, Set<StaffProject> staffProjects) {
+        this.projectId = projectId;
+        this.projName = projName;
+        this.cost = cost;
+        this.staffProjects = staffProjects;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_Id")
+    private Integer projectId;
+
+    @Column(name = "project_name")
+    private String projName;
+
+    @Column(name = "cost")
+    private Integer cost;
+
+    @OneToMany(mappedBy = "project")
+    private Set<StaffProject> staffProjects = new HashSet<>();
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public String getProjName() {
+        return projName;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public Set<StaffProject> getStaffProjects() {
+        return staffProjects;
+    }
+
+    public void setProjName(String projName) {
+        this.projName = projName;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public void setStaffProjects(Set<StaffProject> staffProjects) {
+        this.staffProjects = staffProjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "projectId=" + projectId +
+                ", projName='" + projName + '\'' +
+                ", cost=" + cost +
+                ", staffProjects=" + staffProjects +
+                '}';
+    }
 }
