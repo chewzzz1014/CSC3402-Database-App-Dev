49b39ee8bd5f4d33e95580a91759a818c7a2b717
chewzzz <chewziqing@gmail.com>
2023-06-01 14:43:38 +0800

    add Project and StaffProject repo

--- Staff/src/main/java/com/csc3402/lab/staff/repository/StaffProjectRepository.java
+++ Staff/src/main/java/com/csc3402/lab/staff/repository/StaffProjectRepository.java
@@ -1,4 +1,14 @@
 package com.csc3402.lab.staff.repository;
 
-public class StaffProjectRepository {
+import jakarta.persistence.*;
+import com.csc3402.lab.staff.model.StaffProjectId;
+import com.csc3402.lab.staff.model.StaffProject;
+import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface StaffProjectRepository extends JpaRepository<StaffProject, StaffProjectId> {
+    @Query(value = "SELECT * FROM staff_project where staff_id = :id", nativeQuery = true)
+    List<StaffProject> findStaffProjectByStaffId(@Param("id") int id);
 }
