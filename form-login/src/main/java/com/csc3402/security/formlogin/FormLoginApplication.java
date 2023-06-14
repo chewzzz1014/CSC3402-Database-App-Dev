883fb02a6a406eefcfd1fb4139eac8e9f8257171
chewzzz <chewziqing@gmail.com>
2023-06-14 18:39:48 +0800

    disable security auto config

--- form-login/src/main/java/com/csc3402/security/formlogin/FormLoginApplication.java
+++ form-login/src/main/java/com/csc3402/security/formlogin/FormLoginApplication.java
@@ -1,9 +1,10 @@
 package com.csc3402.security.formlogin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityException.class, ReactiveManagementWebSecurityAutoConfiguration.class})
 public class FormLoginApplication {
 
     public static void main(String[] args) {
